6186fc17c0df31596c86fb7cf704033a346aabdf
QHDuan <mail@qhduan.com>
2018-03-25 04:44:16 +0800

    first commit

---
+++ s3_train.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+import pickle
+import numpy as np
+from tqdm import tqdm
+from keras import layers, models
+from keras import callbacks, optimizers
+
+from utils import MAX_LENGTH, LABEL_DICT
+
+
+def build_model(shape, vocab_size):
+    """构建模型
+    """
+    input_layer = layers.Input(shape=shape)
+
+    m = input_layer
+    m = layers.Embedding(vocab_size, 300)(m)
+    
+    cnn_shape = (1, 2, 3, 4, 5, 6)
+    cnns = []
+    for i in cnn_shape:
+        cnn = m
+        cnn = layers.Conv1D(4, i, padding='same', activation='relu')(cnn)
+        cnn = layers.Conv1D(1, 1, padding='same', activation='linear')(cnn)
+        cnns.append(cnn)
+    
+    cnns = layers.Concatenate()(cnns)
+    cnns = layers.Flatten()(cnns)
+
+    atten = cnns
+    atten = layers.Dense(len(cnn_shape) * shape[0], activation='softmax')(atten)
+
+    cnns = layers.Activation('relu')(cnns)
+    cnns = layers.Multiply()([cnns, atten])
+
+    m = cnns
+    m = layers.Dense(100, activation='linear')(m)
+    m = layers.BatchNormalization()(m)
+    m = layers.Activation('relu')(m)
+    m = layers.Dropout(0.5)(m)
+    m = layers.Dense(100, activation='linear')(m)
+    m = layers.BatchNormalization()(m)
+    m = layers.Activation('relu')(m)
+    m = layers.Dropout(0.5)(m)
+    m = layers.Dense(len(LABEL_DICT), activation='softmax')(m)
+
+    atten_model = models.Model(
+        inputs=input_layer,
+        outputs=atten
+    )
+
+    model = models.Model(
+        inputs=input_layer,
+        outputs=m
+    )
+
+    optimizer = optimizers.Adam(lr=0.001, clipnorm=5.)
+
+    model.compile(
+        optimizer,
+        'categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    model.summary()
+
+    return model, atten_model
+
+
+def main():
+    """训练
+    """
+
+    x_train, x_test, y_train, y_test, class_weight = pickle.load(open('train_data.pkl', 'rb'))
+    ws = pickle.load(open('ws.pkl', 'rb'))
+
+    input_shape = x_train[0].shape
+    model, atten_model = build_model(input_shape, len(ws))
+
+    nb_epoch = 20
+    batch_size = 64
+    steps_per_epoch = int(len(x_train) / batch_size) + 1
+    validation_steps = int(len(x_test) / batch_size) + 1
+
+    model.save_weights('./model_last_weights.hdf5')
+    atten_model.save_weights('./model_last_weights.hdf5')
+
+    model.fit_generator(
+        generator=batch_flow(
+            x_train, y_train, batch_size=batch_size
+        ),
+        steps_per_epoch=steps_per_epoch,
+        epochs=nb_epoch,
+        validation_data=batch_flow(
+            x_test, y_test, batch_size=batch_size
+        ),
+        validation_steps=validation_steps,
+        class_weight=class_weight,
+        callbacks=[
+            callbacks.ReduceLROnPlateau(min_lr=1e-6),
+            callbacks.ModelCheckpoint(
+                monitor='val_acc',
+                filepath='./model_weights.hdf5',
+                verbose=1,
+                save_best_only=True)
+        ]
+    )
+
+    model.save_weights('./model_last_weights.hdf5')
+    atten_model.save_weights('./model_last_weights.hdf5')
+
+
+def batch_flow(inputs, targets, batch_size=32):
+    """流动数据流
+    """
+
+    x_batch, y_batch = [], []
+
+    while True:
+
+        if len(x_batch) == batch_size:
+            x_batch, y_batch = np.array(x_batch), np.array(y_batch)
+            yield x_batch, y_batch
+            x_batch, y_batch = [], []
+
+        ind = np.random.randint(0, len(inputs))
+        x = inputs[ind]
+        y = targets[ind]
+        x_batch.append(x)
+        y_batch.append(y)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,11 @@
+
+MAX_LENGTH = 200
+LABEL_DICT = {
+    'happy': 0,
+    'sad': 1,
+    'angry': 2,
+    'disgust': 3,
+    'fear': 4,
+    'suprise': 5,
+    'neutral': 6,
+}
